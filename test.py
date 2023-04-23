9d9d7ae532df7d71435bb01eb45939c57d6213e2
xukecheng <cnxukecheng@gmail.com>
2023-04-23 09:09:42 +0800

    * feat(test.py): add openai callback to track usage statistics and print them after running the agent. Add comments to the code.

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from langchain.chat_models import ChatOpenAI
 
 from apitable_toolkit.toolkit import APITableToolkit
 from apitable_toolkit.utilities.apitable import APITableAPIWrapper
+from langchain.callbacks import get_openai_callback
 
 os.environ["OPENAI_API_KEY"] = "sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
@@ -21,14 +22,21 @@ agent = initialize_agent(
     agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
     verbose=True,
 )
-# agent.run("What spaces do I have")
-# agent.run("What datasheets do I have")
 
-# TODO: need to make AI understand what is node.
-# TODO: need to make AI do not make up tools.
+with get_openai_callback() as cb:
+    agent.run("What spaces do I have")
+    # agent.run("What datasheets do I have")
 
-# agent.run(
-#     "The space_id is spctqtTZpssYw. Tell me the datasheet ID whose name has 'MAU' in this space"
-# )
+    # TODO: need to make AI understand what is node.
+    # TODO: need to make AI do not make up tools.
+    # agent.run(
+    #     "The space_id is spctqtTZpssYw. Tell me the datasheet ID whose name has 'MAU' in this space"
+    # )
 
-agent.run("Tell me the latest value of APITable MAU in xukecheng's space")
+    # agent.run("Tell me the latest value of APITable MAU in xukecheng's space")
+    # agent.run("Create all field type fields in AITEST of xukecheng's space")
+    print(f"Total Tokens: {cb.total_tokens}")
+    print(f"Prompt Tokens: {cb.prompt_tokens}")
+    print(f"Completion Tokens: {cb.completion_tokens}")
+    print(f"Successful Requests: {cb.successful_requests}")
+    print(f"Total Cost (USD): ${cb.total_cost}")
